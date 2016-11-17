bd98060720d0bce8666d42715aacbc542a149129
1241810 <alexys.leclerc@gmail.com>
2016-11-17 11:50:01 -0500

    Programme qui prend les noms du fichier prenoms.txt, pour les lire et les trier dans le fichier sortie.txt

---
+++ OrdrePrenom/frmPrenom.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OrdrePrenom
+{
+    partial class frmPrenom
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrierLesNoms = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTrierLesNoms
+            // 
+            this.btnTrierLesNoms.Location = new System.Drawing.Point(56, 12);
+            this.btnTrierLesNoms.Name = "btnTrierLesNoms";
+            this.btnTrierLesNoms.Size = new System.Drawing.Size(100, 23);
+            this.btnTrierLesNoms.TabIndex = 0;
+            this.btnTrierLesNoms.Text = "Trier les prénoms";
+            this.btnTrierLesNoms.UseVisualStyleBackColor = true;
+            this.btnTrierLesNoms.Click += new System.EventHandler(this.btnTrierLesNoms_Click);
+            // 
+            // frmPrenom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 49);
+            this.Controls.Add(this.btnTrierLesNoms);
+            this.Name = "frmPrenom";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrierLesNoms;
+    }
+}
+
---
+++ OrdrePrenom/frmPrenom.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OrdrePrenom
+{
+    public partial class frmPrenom : Form
+    {
+        public frmPrenom()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrierLesNoms_Click(object sender, EventArgs e)
+        {
+            const string VOYELLES = "aeiouyAEIOUYéÉ";
+            List<string> nomAvecDeuxVoyelles = new List<string>();
+            int nbVoyelles = 0;
+
+            const string NOM_FICHIER_A_LIRE = @"prenoms.txt";
+            const string NOM_FICHIER_A_ECRIRE = @"sortie.txt";
+
+            try
+            {
+                FileStream fichierALire = new FileStream(NOM_FICHIER_A_LIRE, FileMode.Open, FileAccess.Read);
+                StreamReader lectureDeFichier = new StreamReader(fichierALire);
+                string prenomLuParFichier;
+
+                while ((prenomLuParFichier = lectureDeFichier.ReadLine()) != null)
+                {
+                    for (int i = 0; i < prenomLuParFichier.Length; i++)
+                    {
+                        if (VOYELLES.Contains(prenomLuParFichier[i]))
+                        {
+                            nbVoyelles++;
+                        }
+                    }
+
+                    if (nbVoyelles % 2 == 0)
+                    {
+                        nomAvecDeuxVoyelles.Add(prenomLuParFichier);
+                    }
+                    nbVoyelles = 0;
+                }
+                nomAvecDeuxVoyelles.Sort();
+                File.WriteAllLines(NOM_FICHIER_A_ECRIRE, nomAvecDeuxVoyelles);
+                lectureDeFichier.Close();
+                fichierALire.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Il y a eu une erreur lors de l'exécution du programme. Voici l'erreur : " + exception.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
